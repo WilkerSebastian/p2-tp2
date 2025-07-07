c750963d38019bcf235ddb218ccacc5897dd6a99
WilkerSebastian <wilkerafonso2005@gmail.com>
2025-07-07 12:43:38 -0400

    up: using concepts and MeshAttributes specialization to allow optional attributes

--- include/MeshAttribute.h
+++ include/MeshAttribute.h
@@ -13,56 +13,165 @@ namespace tcii::cg {
     template <typename... Fields>
     using ElementAttribute = SoA<DefaultSoAAllocator, MeshIndex, Fields...>;
 
+    template <typename T>
+    struct is_element_attribute : std::false_type {};
+
+    template <typename... Fields>
+    struct is_element_attribute<SoA<DefaultSoAAllocator, MeshIndex, Fields...>> : std::true_type {};
+
+    template <typename T>
+    inline constexpr bool is_element_attribute_v = is_element_attribute<T>::value;
+
+    template <typename T>
+    concept Defined = is_element_attribute_v<T>;
+
     template <typename VA, typename TA>
-    class MeshAttribute: public SharedObject {
+    class MeshAttribute;
+
+    template <typename VA, typename TA>
+        requires Defined<VA> && Defined<TA>
+    class MeshAttribute<VA, TA> : public SharedObject {
+        
+        public:
+
+            template<size_t I> 
+            auto& vertexAttribute(MeshIndex i) const { 
+                return _va.template get<I>(i); 
+            }
+
+            template<size_t I, typename Field>
+            void setVertexAttribute(MeshIndex i, Field&& field) { 
+                _va.template get<I>(i) = std::forward<Field>(field); 
+            }
+            
+            template <typename... Fields> 
+            void setVertexAttributes(MeshIndex i, Fields&&... fields) { 
+                _va.set(i, std::forward<Fields>(fields)...); 
+            }
+            
+            template<size_t I> 
+            auto& triangleAttribute(MeshIndex i) const { 
+                return _ta.template get<I>(i); 
+            }
+
+            template<size_t I, typename Field>
+            void setTriangleAttribute(MeshIndex i, Field&& field) { 
+                _ta.template get<I>(i) = std::forward<Field>(field); 
+            }
+            
+            template <typename... Fields> 
+            void setTriangleAttributes(MeshIndex i, Fields&&... fields) { 
+                _ta.set(i, std::forward<Fields>(fields)...); 
+            }
+            
+            static ObjectPtr<MeshAttribute> New(const TriangleMesh& mesh) { 
+                return new MeshAttribute(mesh); 
+            }
+        
+        private:
+            
+            ObjectPtr<TriangleMesh> _mesh;
+            VA _va;
+            TA _ta;
+
+            MeshAttribute(const TriangleMesh& mesh) : 
+            _mesh{ &mesh }, 
+            _va{ mesh.data().vertexCount() }, 
+            _ta{ mesh.data().triangleCount() } 
+            {}
+    
+    };
+
+    template <typename VA, typename TA>
+        requires std::is_void_v<VA> && Defined<TA>
+    class MeshAttribute<VA, TA> : public SharedObject {
+    
+        public:
+
+            template<size_t I> 
+            auto& triangleAttribute(MeshIndex i) const { 
+                return _ta.template get<I>(i); 
+            }
+
+            template<size_t I, typename Field>
+            void setTriangleAttribute(MeshIndex i, Field&& field) { 
+                _ta.template get<I>(i) = std::forward<Field>(field); 
+            }
+
+            template <typename... Fields> 
+            void setTriangleAttributes(MeshIndex i, Fields&&... fields) { 
+                _ta.set(i, std::forward<Fields>(fields)...); 
+            }
+
+            static ObjectPtr<MeshAttribute> New(const TriangleMesh& mesh) { 
+                return new MeshAttribute(mesh); 
+            }
+        
+        private:
+
+            ObjectPtr<TriangleMesh> _mesh;
+            TA _ta;
+
+            MeshAttribute(const TriangleMesh& mesh) : 
+            _mesh{ &mesh }, 
+            _ta{ mesh.data().triangleCount() } 
+            {}
+    
+        };
+
+    template <typename VA, typename TA> 
+        requires Defined<VA> && std::is_void_v<TA>
+    class MeshAttribute<VA, TA> : public SharedObject {
+        
+        public:
+
+            template<size_t I> 
+            auto& vertexAttribute(MeshIndex i) const { 
+                return _va.template get<I>(i); 
+            }
+            
+            template<size_t I, typename Field>
+            void setVertexAttribute(MeshIndex i, Field&& field) { 
+                _va.template get<I>(i) = std::forward<Field>(field); 
+            }
+
+            template <typename... Fields> void setVertexAttributes(MeshIndex i, Fields&&... fields) { 
+                _va.set(i, std::forward<Fields>(fields)...); 
+            }
+
+            static ObjectPtr<MeshAttribute> New(const TriangleMesh& mesh) { 
+                return new MeshAttribute(mesh); 
+            }
+
+        private:
+        
+            ObjectPtr<TriangleMesh> _mesh;
+            VA _va;
+
+            MeshAttribute(const TriangleMesh& mesh) :
+            _mesh{ &mesh }, 
+            _va{ mesh.data().vertexCount() } 
+            {}
+
+    };
+
+    template <typename VA, typename TA>
+        requires std::is_void_v<VA> && std::is_void_v<TA>
+    class MeshAttribute<VA, TA> : public SharedObject {
 
         public:
 
             static ObjectPtr<MeshAttribute> New(const TriangleMesh& mesh) {
                 return new MeshAttribute(mesh);
-            } 
-
-            template<size_t I>
-            auto& vertexAttribute(MeshIndex i) const {
-                return _va.template get<I>(i);
             }
 
-            template <size_t I, typename Field>
-            void setVertexAttribute(MeshIndex i, Field&& field) {
-                _va.template get<I>(i) = std::forward<Field>(field);
-            }
-
-            template <typename... Fields>
-            void setVertexAttributes(MeshIndex i, Fields&&... fields) {
-                _va.set(i, std::forward<Fields>(fields)...);
-            }
-
-            template<size_t I>
-            auto& triangleAttribute(MeshIndex i) const {
-                return _ta.template get<I>(i);
-            }
-
-            template <size_t I, typename Field>
-            void setTriangleAttribute(MeshIndex i, Field&& field) {
-                _ta.template get<I>(i) = std::forward<Field>(field);
-            }
-
-            template <typename... Fields>
-            void setTriangleAttributes(MeshIndex i, Fields&&... fields) {
-                _ta.set(i, std::forward<Fields>(fields)...);
-            }
-            
         private:
 
             ObjectPtr<TriangleMesh> _mesh;
-            VA _va;
-            TA _ta;
 
-            MeshAttribute(const TriangleMesh& mesh): 
-            _mesh{&mesh},
-            _va{mesh.data().vertexCount()},
-            _ta{mesh.data().triangleCount()} 
-            {};
+            MeshAttribute(const TriangleMesh& mesh) :
+            _mesh{ &mesh } 
+            {}
 
     };
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -6,18 +6,12 @@ using namespace tcii::cg;
 auto decorate(const TriangleMesh& mesh) {
 
   using Color = Vec3f;
-  using VA = ElementAttribute<Color>;
+  using VA = void;
   using TA = ElementAttribute<Color>;
   using MA = MeshAttribute<VA, TA>;
 
-  auto nv = mesh.data().vertexCount();
   auto nt = mesh.data().vertexCount();
   auto ma = MA::New(mesh);
-
-  for (decltype(nv) i = 0; i < nv; ++i)
-    ma->setVertexAttributes(i, Color{0, 0, 1});
-
-  ma->setVertexAttribute<0>(0, Color{1, 1, 0});
 
   for (decltype(nt) i = 0; i < nt; ++i)
     ma->setTriangleAttributes(i, Color{0, 1, 0});
@@ -43,7 +37,6 @@ main()
 
   auto attributes = decorate(*mesh);
 
-  std::cout << attributes->vertexAttribute<0>(0) << '\n';
   std::cout << attributes->triangleAttribute<0>(0) << '\n';
 
   puts("Press any key to exit...");
